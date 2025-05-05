434e72aebef9beb78e8665184e754101724ff199
baoshuangzhang <zhangbs.san@gmail.com>
2025-05-05 10:48:13 +0200

    Initial commit

---
+++ data_filter.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from pycaret.classification import *
+import matplotlib.pyplot as plt
+import streamlit as st
+
+def data_preprocess_filter_data(start_year:int, end_year:int, filtered_feature:str, filtered_value:str):
+    # Load the data
+    data = pd.read_csv('monatszahlen2501_verkehrsunfaelle_27_02_25.csv')
+
+    # Get the time period of data
+    data_clip = data[(data['JAHR']<=end_year)& (data['JAHR']>=start_year)]
+    # Create a DataFrame with specific columns
+    df = pd.DataFrame(data_clip, columns=['MONATSZAHL', 'AUSPRAEGUNG', 'JAHR', 'MONAT', 'WERT'])
+    # Filter the DataFrame for a specific condition
+    df = df[df[filtered_feature] == filtered_value]
+    #  # Save the filtered DataFrame to a CSV file
+    # df.to_csv(file_name, index=False)
+    return df
---
+++ main.py
@@ -0,0 +1,63 @@
+import pandas as pd
+from pycaret.classification import *
+import matplotlib.pyplot as plt
+import streamlit as st
+from sklearn.preprocessing import MinMaxScaler
+from pytorch_forecasting import TimeSeriesDataSet
+from pytorch_forecasting.models import TemporalFusionTransformer
+import lightning.pytorch as pl
+from lightning.pytorch.callbacks import ModelCheckpoint
+import data_filter
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+import numpy as np
+
+app = FastAPI()
+model = TemporalFusionTransformer.load_from_checkpoint("checkpoints/tft-epoch=90-train_loss=4.6115.ckpt")
+
+class Item(BaseModel):
+    # time_idx: int
+    year: str
+    month: str
+    # group: "MONATSZAHL"
+
+# # prepare the data
+df = data_filter.data_preprocess_filter_data(2000, 2022, 'AUSPRAEGUNG', 'insgesamt')
+df = df[df['MONAT'] != 'Summe']
+df.dropna(inplace=True) 
+df = df[df['MONATSZAHL'] == 'Alkoholunfälle']
+# df.set_index('MONAT', inplace=True)
+# sort the data by time
+df = df.sort_values(by=["MONAT"])
+df =df.reset_index(drop=True)
+df['time_idx']=df.index
+
+@app.post("/predict")
+def predict(request: Item):
+    # json_data = await request.json()
+    time_idx_max = (int(request.year)-2000)*12 + int(request.month)-2
+    encoder_data = df[df.time_idx <= time_idx_max]
+    decoder_data = pd.DataFrame({
+    "MONATSZAHL": ["Alkoholunfälle"],
+    'AUSPRAEGUNG': ["insgesamt"],
+    "JAHR": '0',
+    "MONAT": '0',
+    "WERT": [0],
+    "time_idx": [time_idx_max + 1]
+})
+    data = pd.concat([encoder_data,decoder_data], ignore_index=True)
+    dataset = TimeSeriesDataSet(
+    data,
+    time_idx='time_idx',
+    target='WERT',
+    group_ids=["MONATSZAHL"],
+    max_encoder_length=time_idx_max+1,
+    max_prediction_length=1,
+    time_varying_known_reals=["time_idx"],
+    time_varying_unknown_reals=["WERT"]
+)
+    dataloader = dataset.to_dataloader(train=False, batch_size=1)
+    prediction = model.predict(dataloader, mode="prediction", return_index=False, return_x=False)
+    final = int(prediction.item())
+    return {"prediction": final}
+    # return {"prediction":time}
